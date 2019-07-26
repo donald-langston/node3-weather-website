d01bd3afc49f3e75070b4c5a1baf9eef5a24f35a
Donald Langston <donald_langston@yahoo.com>
2019-07-26 00:57:53 -0400

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined);
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.'
-                        + ' There is a ' + body.currently.precipProbability + '% chance of rain.');
+                        + ' There is a ' + body.currently.precipProbability + '% chance of rain. ' +
+                        'The high for today is ' + body.daily.data[0].temperatureHigh + ' degrees and the low is ' + body.daily.data[0].temperatureLow + ' degrees.');
                 }
     });
 };
